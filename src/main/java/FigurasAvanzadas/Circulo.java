551b3228f2a18bffb9f8d160a8a9e311e4904e9f
Violeta Rodriguez <55716688+violeta1391@users.noreply.github.com>
2020-09-25 09:58:27 -0300

    modifique rectangulo

--- src/main/java/FigurasAvanzadas/Circulo.java
+++ src/main/java/FigurasAvanzadas/Circulo.java
@@ -21,6 +21,29 @@ public class Circulo extends FiguraGeometrica {
         this.radio = diametro;
     }
 
+    public Circulo (int radio) {
+        setRadio(radio);
+        determinarColor();
+    }
+
+    // Determina el color del circulo segun el radio
+
+    private void determinarColor() {
+        if (getRadio() < 100) {
+            setColor(Color.BLUE);
+        } else if (getRadio() < 200) {
+            setColor(Color.GREEN);
+        } else if (getRadio() < 300) {
+            setColor(Color.YELLOW);
+        } else if (getRadio() < 400) {
+            setColor(Color.ORANGE);
+        } else if (getRadio() < 500) {
+            setColor(Color.RED);
+        } else {
+            setColor(Color.MAGENTA);
+        }
+    }
+
     /**
      * Calcula el perimetro de la figura.
      * @return El perimetro.
@@ -48,6 +71,7 @@ public class Circulo extends FiguraGeometrica {
     public void setRadio(Integer radio) {
         if ((0 < radio) && (radio < MAX_RADIO)) {
             this.radio = radio;
+            determinarColor();
         }
     }
 
